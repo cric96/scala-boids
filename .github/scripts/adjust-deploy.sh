d6ad2ba2a6706fbf96ef9e8b5cb02dcf2f49646b
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2022-08-08 01:26:21 +0200

    ci: adjust pub script

--- .github/scripts/adjust-deploy.sh
+++ .github/scripts/adjust-deploy.sh
@@ -3,6 +3,6 @@ mkdir ./public
 mkdir ./public/target
 mkdir ./public/target/scala-3.1.3
 
-cp ./boids/js/target/scala-3.1.3/scala-boids-fastopt.js ./public/target/scala-3.1.3/scala-boids-fastopt.js
+cp ./boids/js/target/scala-3.1.3/scala-boids-fastopt/main.js ./public/target/scala-3.1.3/scala-boids-fastopt/main.js
 cp ./boids/js/index.html ./public/index.html
 cp ./boids/js/style.css ./public/style.css
