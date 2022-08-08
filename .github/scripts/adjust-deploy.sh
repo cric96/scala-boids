0d6b690e2bed975291fb639436fd9fc1e567d455
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2022-08-08 15:36:16 +0200

    chore: fix deploy

--- .github/scripts/adjust-deploy.sh
+++ .github/scripts/adjust-deploy.sh
@@ -3,7 +3,9 @@ mkdir ./public
 mkdir ./public/target
 mkdir ./public/target/scala-3.1.3/
 mkdir ./public/target/scala-3.1.3/scalajs-bundler
+mkdir ./public/target/scala-3.1.3/scalajs-bundler/main
 
-cp ./boids/js/target/scala-3.1.3/scalajs-bundle/scala-boids-fastopt-bundle.js ./public/target/scala-3.1.3/scala-boids-fastopt/
+
+cp ./boids/js/target/scala-3.1.3/scalajs-bundler/main/scala-boids-fastopt-bundle.js ./public/target/scala-3.1.3/scalajs-bundler/main
 cp ./boids/js/index.html ./public/index.html
 cp ./boids/js/style.css ./public/style.css
