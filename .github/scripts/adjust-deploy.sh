fc94f6fe4a172c8e2d6118954a91e5fd87d7bd34
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2025-06-05 18:03:47 +0200

    chore: update scala boids project

--- .github/scripts/adjust-deploy.sh
+++ .github/scripts/adjust-deploy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 mkdir ./public
 mkdir ./public/target
-mkdir ./public/target/scala-3.1.3/
-mkdir ./public/target/scala-3.1.3/scalajs-bundler
-mkdir ./public/target/scala-3.1.3/scalajs-bundler/main
+mkdir ./public/target/scala-3.3.3/
+mkdir ./public/target/scala-3.3.3/scalajs-bundler
+mkdir ./public/target/scala-3.3.3/scalajs-bundler/main
 
 
-cp ./boids/js/target/scala-3.1.3/scalajs-bundler/main/scala-boids-fastopt-bundle.js ./public/target/scala-3.1.3/scalajs-bundler/main
+cp ./boids/js/target/scala-3.3.3/scalajs-bundler/main/scala-boids-fastopt-bundle.js ./public/target/scala-3.3.3/scalajs-bundler/main
 cp ./boids/js/index.html ./public/index.html
 cp ./boids/js/style.css ./public/style.css
